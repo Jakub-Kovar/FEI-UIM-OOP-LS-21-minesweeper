92586a36c225f513303a1222fb61ef22c06b9ed0
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-15 18:17:32 +0100

    game refactor

--- src/main/java/sk/stuba/fei/uim/oop/game/Game.java
+++ src/main/java/sk/stuba/fei/uim/oop/game/Game.java
@@ -14,19 +14,25 @@ public class Game {
     public void play() {
         try {
             while(!this.board.isBoardRevealed()) {
-                System.out.println(this.board.draw());
+                drawBoard();
                 this.board.reveal(getNextMove());
             }
         } catch (MineRevealedException e) {
             System.out.println("You stepped on a mine!");
-            System.out.println(this.board.draw());
+            drawBoard();
+            return;
         }
         System.out.println("You have revealed the whole board!");
+        drawBoard();
     }
 
     private Move getNextMove() {
         char row = ZKlavesnice.readChar(String.format("Enter row (a-%s)", (char)('a' + Board.BOARD_SIZE - 1)));
         int col = ZKlavesnice.readInt(String.format("Enter column (0-%d)", Board.BOARD_SIZE - 1));
         return new Move(col, row);
+    }
+
+    private void drawBoard() {
+        System.out.println(this.board.draw());
     }
 }
